2f0f460f81dc12ca993d7dd058c8b2cad362d99e
Chris Latham <chrislatham@Chriss-iMac.local>
2025-08-14 16:25:46 +0100

    Fix build error - remove deprecated config export from API route

--- :next.config.js
+++ :next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   images: {
     domains: ['images.clerk.dev', 'img.clerk.com'],
   },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '100mb',
-    },
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '100mb',
-    },
-  },
 }
 
 module.exports = nextConfig
