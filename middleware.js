da015f73c973e42151e7a6777b271517b8084624
Chris Latham <chrislatham@Chriss-iMac.local>
2025-08-31 10:50:00 +0100

    Force redeploy to clear Clerk env cache

--- middleware.js
+++ middleware.js
@@ -8,4 +8,4 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
-};
+};// Force redeploy Sun 31 Aug 2025 10:49:52 BST
